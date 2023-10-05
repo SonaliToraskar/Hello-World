9f29e3239703d7a7ba8a8e21b8429a0f690ae598
SonaliToraskar <146720176+SonaliToraskar@users.noreply.github.com>
2023-10-05 16:37:01 +0530

    Add files via upload

---
+++ Hello  World/src/Main/FristProgram.java
@@ -0,0 +1,19 @@
+package Main;
+
+public class FristProgram {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        char c = 'a';
+        byte byteDatatype = 123;
+        short s = 3456;
+        int i = 23444;
+        long l = 234445345;
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(byteDatatype);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+    }
+}
